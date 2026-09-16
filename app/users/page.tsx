21a05b1a3b162352a7193981dd1724a561a4d0a1
Diane Uwamariya <dianahporter@gmail.com>
2026-09-16 22:09:13 +0200

    feat: caching and rendering in Nextjs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User{
+    id: number,
+    name: string
+}
+
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'});
+    const users: User[] = await res.json();
+    console.log(users)
   return (
-    <div>
-      UsersPage
-    </div>
+    <>
+    <h1 className='text-3xl text-pink-800'>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
+    <ol>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ol>
+
+    </>
   )
 }
 
